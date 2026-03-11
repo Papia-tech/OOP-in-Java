ad94e3f7db609e7fa5fa4a2f56021612d8ebb107
Papia Karmakar <karmakarpapia06@gmail.com>
2026-03-11 12:43:55 +0530

    Refactor Student class with constructor chaining
    Refactored Student class to include no-arg and 1-parameter constructors, demonstrating constructor chaining. Updated main method to create Student instance using no-arg constructor.

--- constructor/chaining from another class/Student.java
+++ constructor/chaining from another class/Student.java
@@ -1,22 +1,42 @@
-//using inheritance
+//By using inheritance
 //but constructor name will be different
 
-class Person {
-    String name;
+//Constructor chaining is when one constructor calls another constructor in the same class or in the parent class.
+//Within the same class: Use this()
+//From parent class (superclass): Use super()
 
-    Person(String name) {
+//Can not declare both file as public. Only one file is public
+class Person {
+    int id;
+    String name;
+    
+    // 2-parameter constructor
+    Person(int id, String name) {
+        this.id = id;
         this.name = name;
-        System.out.println("This is parent constructor"); // executed first
+        System.out.println("2-parameter constructor called");
+        System.out.println("ID: " + id + ", Name: " + name);
     }
 }
 
-class Student extends Person {
+public class Student extends Person{
     int id;
+    String name;
 
-    Student(int id, String name) {
-        super(name); // calls Person constructor first
-        this.id = id;
-        System.out.println("This is child constructor"); // executed after parent
+    // No-arg constructor calls 1-parameter constructor
+    Student() {
+        this(0);
+        System.out.println("No-arg constructor called");
+        System.out.println("Default ID: " + id);
+        id=2;
+        name="Papia";//This will be displayed when we call the display method in the main method. It will override the default values set by the no-arg constructor.
+    }
+
+    // 1-parameter constructor calls 2-parameter constructor
+    Student(int id) {
+        super(id, "Unknown");
+        System.out.println("1-parameter constructor called");
+        System.out.println("ID: " + id);
     }
 
     void display() {
@@ -24,7 +44,16 @@ class Student extends Person {
     }
 
     public static void main(String[] args) {
-        Student s = new Student(101, "Alex");
+        Student s = new Student();       
         s.display();
     }
 }
+
+//Output: 
+// 2-parameter constructor called
+// ID: 0, Name: Unknown
+// 1-parameter constructor called
+// ID: 0
+// No-arg constructor called
+// Default ID: 0
+// ID: 2, Name: Papia
