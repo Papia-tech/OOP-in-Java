8daa5e94c39dac95fec5372ed671d5f7cd486343
Papia Karmakar <karmakarpapia06@gmail.com>
2026-04-12 09:04:43 +0530

    Enhance comments in Student.java
    Clarified comments regarding methods and constructors, and added explanation for method calling.

--- 3. Method/1. BasicMethodCreation/Student.java
+++ 3. Method/1. BasicMethodCreation/Student.java
@@ -1,8 +1,24 @@
+// A method is a block of code that performs a specific task and can be called whenever needed.
+// In general programming → Method and fuction are almost same, In Java (OOP) → Slight difference
+//    --A function is a block of code that: Performs a task, Usually independent (not tied to a class/object)
+//    --A method is a function that: Belongs to a class, Works with objects
+
+// Difference between method and constructor ->
+//    --Constructor → used to initialize an object
+//    --Method → used to perform operations/tasks
+//    --A method must have a return type (including void), whereas a constructor has no return type
+
+// Why use method?
+//    --Reusability → write once, use many times
+//    --Clean code → organized structure
+//    --Easy understanding
+//    --Easy debugging
+
 class Student {
     int id;
     String name;
 
-    void show() {   // no return type, no parameters
+    void show() {   // return type is void (does not return value), no parameters
         System.out.println(id + " " + name);
     }
 
@@ -10,7 +26,7 @@ class Student {
         Student s = new Student();
         s.id = 1;
         s.name = "Alex";
-        s.show();
+        s.show(); // Method calling
     }
 }
 
