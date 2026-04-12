61b67aa1b3990b3cd8fc177c3ee40178703e8ea7
Papia Karmakar <karmakarpapia06@gmail.com>
2026-04-12 08:24:37 +0530

    Refine comments on constructor overloading in Student.java
    Updated comments to clarify constructor overloading rules and benefits.

--- 2. Constructor/6. ConstructorOverloading/Student.java
+++ 2. Constructor/6. ConstructorOverloading/Student.java
@@ -1,6 +1,15 @@
-//Constructor overloading means having more than one constructor in a class with different parameter lists.
-//Same class → multiple constructors
-//Different number or type of parameters
+// Constructor Overloading means having more than one constructor in a class with different parameter lists.
+// Same class → multiple constructors
+
+// Why use Overloading?
+//    --Constructor overloading allows creating objects in different ways depending on available data.
+//    --Constructor overloading improves "flexibility" by allowing objects to be initialized in multiple ways.
+
+// Rules of Constructor Overloading ->
+//    --All constructors must have the same name as the class
+//    --Different Parameter List (Constructors must differ in: Number of parameters or Type of parameters or Order of parameters)
+//    --No Limit on Number of Constructors
+//    --Cannot Overload by Return Type (constructors don’t have return types)
 
 class Student {
     int id;
@@ -39,7 +48,7 @@ class Student {
     }
 }
 
-//Output::
-//ID: 0, Name: Unknown
-//ID: 101, Name: Unknown
-//ID: 102, Name: Alex
+// Output::
+// ID: 0, Name: Unknown
+// ID: 101, Name: Unknown
+// ID: 102, Name: Alex
